7ef69fabcd685c366799ccf5080df3b3b6204ff9
Vijo Cherian <vcherian@eridan.io>
2022-12-12 14:10:41 -0800

    Made client chagnes work

--- ui/split8/client/ecmctl.c
+++ ui/split8/client/ecmctl.c
@@ -12,10 +12,9 @@
 
 #define MAXLINE 1024 
 
-ecm_ctrl_t
-connect_to_server()
+int
+connect_to_server(struct sockaddr_in *servaddr)
 {
-    struct sockaddr_in     servaddr;
     int sockfd;
 
     // Creating socket file descriptor 
@@ -24,25 +23,26 @@ connect_to_server()
         exit(EXIT_FAILURE); 
     }
 
-    memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(servaddr, 0, sizeof(servaddr));
         
     // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(EC_SERVER_PORT); 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr->sin_family = AF_INET;
+    servaddr->sin_port = htons(EC_SERVER_PORT);
+    servaddr->sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    return ECM_SUCCESS;
+    return sockfd;
 }
 
 int main()
 { 
+    struct sockaddr_in     servaddr;
     char buffer[MAXLINE]; 
-    eridan_cmd_hdr_t *hdr;
+    eridan_cmd_hdr_t *hdr, *reply;
     eridan_cmd_req_t *req;
 
     int n, len;
 
-    int sockfd = connect_to_server();
+    int sockfd = connect_to_server(&servaddr);
 
     hdr = malloc(sizeof(eridan_cmd_hdr_t));
     memset(hdr, 0, sizeof(eridan_cmd_hdr_t));
@@ -54,19 +54,21 @@ int main()
     req->reqid   = 0x3434;
     req->cmdid   = ERIDAN_CMD_GET_VERSION;
 
-    sendto(sockfd,  (const char *)hdr, sizeof(eridan_cmd_hdr_t), 
+    sendto(sockfd,  (const char *)hdr, sizeof(eridan_cmd_hdr_t),
                     MSG_CONFIRM,
                     (const struct sockaddr *) &servaddr, sizeof(servaddr));
-    sendto(sockfd,  (const char *)req, sizeof(eridan_cmd_req_t), 
+    sendto(sockfd,  (const char *)req, sizeof(eridan_cmd_req_t),
                     MSG_CONFIRM,
-                    (const struct sockaddr *) &servaddr, sizeof(servaddr));             
+                    (const struct sockaddr *) &servaddr, sizeof(servaddr));
     printf("Hello message sent.\n");
 
-    n = recvfrom(sockfd, (char *)buffer, sizeof(eridan_cmd_hdr_t),  
+    len = sizeof(servaddr);
+    n = recvfrom(sockfd, (char *)buffer, sizeof(eridan_cmd_hdr_t),
                          MSG_WAITALL,
                          (struct sockaddr *) &servaddr, (unsigned int *)&len); 
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer); 
+    printf("Recved %d bytes from server\n", n);
+    reply = (eridan_cmd_hdr_t *)buffer;
+    printf("Server : %x\n", reply->cookie);
     
     close(sockfd); 
     return 0; 
