c7a1a8db1ea6257987e4e06e039ce35979e50345
Vijo Cherian <vcherian@eridan.io>
2022-12-13 12:09:47 -0800

    Added handling of each command

--- ui/split8/server/miracle_controller.c
+++ ui/split8/server/miracle_controller.c
@@ -69,6 +69,138 @@ start_server(int port)
 }
 
 ecm_ctrl_t
+handle_sysinit(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_get_freq(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_get_stats(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_set_freq(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_get_pwr(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_set_pwr(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_get_sample_rate(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_set_sample_rate(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_get_rxfreq(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_set_rxfreq(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_get_rxsamplerate(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_set_rxsamplerate(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_get_rxgains(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_set_rxgains(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_sysoff(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_start_scp(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_prep_scp(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_reset_now(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_reset_done(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_send_updates(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_check_updates(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
+handle_get_version(eridan_cmd_req_t *req)
+{
+    return ECM_SUCCESS;
+}
+
+ecm_ctrl_t
 handle_cmds(char *rbuf, int rlen, struct sockaddr *caddr)
 {
     eridan_cmd_hdr_t *hdr, *reply;
@@ -93,10 +225,105 @@ handle_cmds(char *rbuf, int rlen, struct sockaddr *caddr)
 
     printf("Got req from client\n");
 
+    switch (req->cmdid)
+    {
+        case ERIDAN_CMD_SYSINIT:
+            handle_sysinit(req);
+        break;
+
+        case ERIDAN_CMD_GET_FREQ:
+            handle_get_freq(req);
+        break;
+
+        case ERIDAN_CMD_GET_STATS:
+            handle_get_stats(req);
+        break;
+
+        case ERIDAN_CMD_SET_FREQ:
+            handle_set_freq(req);
+        break;
+
+        case ERIDAN_CMD_GET_PWR:
+            handle_get_pwr(req);
+        break;
+
+        case ERIDAN_CMD_SET_PWR:
+            handle_set_pwr(req);
+        break;
+
+        case ERIDAN_CMD_GET_SAMPLE_RATE:
+            handle_get_sample_rate(req);
+        break;
+
+        case ERIDAN_CMD_SET_SAMPLE_RATE:
+            handle_set_sample_rate(req);
+        break;
+
+        case ERIDAN_CMD_GET_RXFREQ:
+            handle_get_rxfreq(req);
+        break;
+
+        case ERIDAN_CMD_SET_RXFREQ:
+            handle_set_rxfreq(req);
+        break;
+
+        case ERIDAN_CMD_GET_RXSAMPLERATE:
+            handle_set_rxsamplerate(req);
+        break;
+
+        case ERIDAN_CMD_SET_RXSAMPLERATE:
+            handle_set_rxsamplerate(req);
+        break;
+
+        case ERIDAN_CMD_GET_RXGAINS:
+            handle_get_rxgains(req);
+        break;
+
+        case ERIDAN_CMD_SET_RXGAINS:
+            handle_set_rxgains(req);
+        break;
+
+        case ERIDAN_CMD_SYSOFF:
+            handle_sysoff(req);
+        break;
+
+        case ERIDAN_CMD_START_SCP:
+            handle_start_scp(req);
+        break;
+
+        case ERIDAN_CMD_PREP_SCP:
+            handle_prep_scp(req);
+        break;
+
+        case ERIDAN_CMD_RESET_NOW:
+            handle_reset_now(req);
+        break;
+
+        case ERIDAN_CMD_RESET_DONE:
+            handle_reset_done(req);
+        break;
+
+        case ERIDAN_CMD_SEND_UPDATES:
+            handle_send_updates(req);
+        break;
+
+        case ERIDAN_CMD_CHECK_UPDATES:
+            handle_check_updates(req);
+        break;
+
+        case ERIDAN_CMD_GET_VERSION:
+            handle_get_version(req);
+        break;
+
+        default:
+        break;
+    }
+
     reply = malloc(sizeof(eridan_cmd_hdr_t));
     memset(reply, 0, sizeof(eridan_cmd_hdr_t));
     reply->cookie  = EC_MAGIC_COOKIE;
-    reply->version = 0;
+    reply->version = EC_VERSION;
+    reply->type    = EC_TYPE_RESP;
     reply->length  = 20;
 
     sendto(udpfd,  (const char *)reply, sizeof(eridan_cmd_hdr_t), 
